e6b1f6bfb8c301d9428f0ec9686915a0a0f3dff9
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2022-10-03 23:22:06 +0700

    feat: styling home

---
+++ src/components/core/Header.tsx
@@ -0,0 +1,21 @@
+import { Stack } from '@mantine/core'
+import React from 'react'
+
+function Header() {
+  return (
+    <Stack
+      sx={{
+        overflow: 'hidden',
+        backgroundColor: '#333',
+        position: 'fixed',
+        bottom: '50%',
+        width: '100%',
+        maxWidth: 'in'
+      }}
+    >
+      test
+    </Stack>
+  )
+}
+
+export default Header
